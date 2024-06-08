03f40cc5648c1bbd01eaa2c94c49449829600fa3
Adhi <adhipathipandiyan.s12@gmail.com>
2024-06-08 09:18:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ContactForm from "./ContactForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="container">
+      <ContactForm />
+    </main>
+  );
+};
+
+export default App;
